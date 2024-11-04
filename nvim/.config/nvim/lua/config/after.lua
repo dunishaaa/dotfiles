29cb396b023a308285aa02e319097fb0d9d63e92
dunishaaa <aphimontes@outlook.com>
2024-11-03 21:17:20 -0600

    cursor line bg

--- nvim/.config/nvim/lua/config/after.lua
+++ nvim/.config/nvim/lua/config/after.lua
@@ -1,45 +1,4 @@
-vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#6272A4', bold=true })
-vim.api.nvim_set_hl(0, 'LineNr', { fg='#BD93F9', bold=true })
-vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#6272A4', bold=true })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#6272A4", bold = true })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#BD93F9", bold = true })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#6272A4", bold = true })
+vim.api.nvim_set_hl(0, "CursorLine", { bold = true, bg = "#1C403E" })
--- nvim/.config/nvim/lua/custom/manim.lua
+++ nvim/.config/nvim/lua/custom/manim.lua
@@ -1,34 +1,16 @@
-local function manimr()
-	local file = vim.cmd.f()
-
-	local command = "manimr"
-
-	local a = vim.fn.expand("%")
-	command = command .. " " .. a
-
-	print("\n")
-	print(command)
-	os.execute(command)
-end
-
 local function manim(flags)
 	local file = vim.fn.expand("%:p")
 	local path = vim.fn.expand("%:h")
 	local command = '"manimr'
-	local section = false
-	local quality = 0
 	for i = 1, #flags do
 		command = command .. " " .. flags[i]
 	end
 
 	command = command .. " " .. file .. '"'
 
-	-- local scene = vim.fn.input("Select your scene > ")
-	--command = command .. " " .. scene .. '"'
 	path = '"' .. path .. '"'
 
 	local conf = "go_back=0 size=40 dir=" .. path .. " " .. "cmd=" .. command
-	--    local conf = "cmd=" .. command
 	print(conf)
 	vim.cmd.TermExec(conf)
 end
